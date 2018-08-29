2579acbeab39fdb320a3dec404a3b789d9505105
Vu Le <thuy.dtt1118@thuys-air.lan>
2018-08-28 23:25:26 -0500

    Finnaly DOnegit add .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,38 +3,45 @@ var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
 var win = 0;
 var lose = 0;
 var remain = 9;
-var comChoice;
 resets();
 
-// var comChoice = letters[Math.floor(Math.random() * letters.length)];
+
+var comChoice = letters[Math.floor(Math.random() * letters.length)];
 
 var guesses = [];
 document.onkeyup = function(event){
     var guess = event.key;
     guesses.push(guess);
     document.querySelector("#guess").innerHTML = guesses;
-    if (guess === comChoice){
-      win++;
-      resets();
+    if (guess == comChoice){
+        win++;
+        winReset();
     } 
-    else {
+    if (guess !== comChoice){
         remain--;
     }
+
     if (remain === 0){
         lose++;
         resets();
-    }    
-    
+    }  
     document.querySelector("#losing").innerHTML = lose;
     document.querySelector("#remainer").innerHTML = remain;
     document.querySelector("#winning").innerHTML = win;
 }
-    
-    function resets () {
-        remain = 9;
-        guesses = [];
-        var comChoice = letters[Math.floor(Math.random() * letters.length)];
-        console.log(comChoice);
-    }
 
+
+function resets () {
+    remain = 9;
+    guesses = [];
+    // var comChoice = letters[Math.floor(Math.random() * letters.length)];
+}
+
+function winReset(){
+    comChoice = letters[Math.floor(Math.random() * letters.length)];
+    console.log(comChoice);
+}
+
+
+console.log(comChoice);
 
